46e1de431970f7756c002d12f708b557879cda3c
Mark Kim <viking617617@gmail.com>
2017-11-01 19:31:18 +0900

    first commit - Crganizing settings/base.py

---
+++ mysite/config/__init__.py
@@ -0,0 +1,5 @@
+#1. member application 생성
+#2. AbstractUser를 상속받은 User 구현
+# User에 img_profile (ImageField) 추가
+#3. AUTH_USER_MODEL = 'member.User'
+# INSTALLED_APPS에 추가
---
+++ mysite/member/admin.py
@@ -0,0 +1,15 @@
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+
+from .models import User
+
+
+class UserAdmin(BaseUserAdmin):
+    fieldsets = BaseUserAdmin.fieldsets + (
+        ('추가 정보', {'fields': (
+            'img_profile',
+        )}),
+    )
+
+
+admin.site.register(User, UserAdmin)
